cff890f7a31193a12d26f67e8ae8470dd7d09e57
SreenathKoyyam <sreenath886@gmail.com>
2017-05-18 19:36:51 +0530

    added action types page and removed all static action type variables

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,11 +1,13 @@
 import React , { PropTypes } from 'react';
 import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 
 
 
 class CoursesPage extends React.Component
 {
+    //constructor to set state
 constructor(props,context){
 
 super(props,context);
@@ -14,10 +16,13 @@ this.state={
         title: ""
     }
 };
+//bind functions 
 this.onTitleChange=this.onTitleChange.bind(this);
 this.onClickSave=this.onClickSave.bind(this);
 
 }
+
+//child function 1 :: called by render
 onTitleChange(event)
 {
     const course = this.state.course;
@@ -25,16 +30,19 @@ onTitleChange(event)
     this.setState({course:course});
 
 }
+//child function 2 :: called by render
 onClickSave(){
 
   //  alert(`Saving${this.state.course.title}` );
- // this.props.dispatch(courseActions.createCourse(this.state.course));
-  this.props.createCourse(this.state.course);
+ // this.props.createCourse(this.state.course);
+ this.props.actions.createCourse(this.state.course);
 }
+//child function 3 :: called by render
 courseRow(course, index){
 return <div key={index}>{course.title} </div>;
 }
 
+//render functon
 render(){
    
 return(
@@ -52,12 +60,17 @@ return(
 }
 
 }
+
+//propsType :: provide proptype validation
 CoursesPage.PropTypes ={
-dispatch: PropTypes.func.isRequired,
+//dispatch: PropTypes.func.isRequired,
 courses: PropTypes.array.isRequired,
-createCourse: PropTypes.func.isRequired
+actions: PropTypes.object.isRequired
 
 };
+
+//Redux-connect related function
+
 function mapStateToProps (state, ownProps){
    // debugger;
     return{
@@ -67,8 +80,9 @@ courses: state.courses
 }
 function mapDispatchToProps(dispatch){
     return{
-createCourse: course =>dispatch(courseActions.createCourse(course))
+//createCourse: course =>dispatch(courseActions.createCourse(course))
+actions: bindActionCreators(courseActions,dispatch)
     };
 }
-
+//export using Redux-connect
 export default connect(mapStateToProps,mapDispatchToProps)(CoursesPage);
